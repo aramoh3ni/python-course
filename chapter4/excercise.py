7db553762940b64ad3212f70dc815146d298765b
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-05-19 15:20:38 +0430

    Chapter 4, Excercise 1 is done.

---
+++ chapter4/excercise.py
@@ -0,0 +1,10 @@
+# Alireza Mohseni
+# Chapter 4
+# NOTE::Loop Excercise
+
+best_friends = ['Atiqullah', "Sadiq", "Jamshid", "Fardin"]
+message = "I love to walk with {}"
+for friend in best_friends:
+    print(message.format(friend.title()))
+
+print("I really love my best friends")
---
+++ chapter4/loop.py
@@ -0,0 +1,44 @@
+# Alireza Mohseni
+# May 19, 2022
+# Python Crash Course
+
+# Loop 
+# allow to us to take the same action or set of action, with every item in a list
+# any length, includeing 1000, items in the lists
+
+# Loop through an entire list
+# magicians = ['alice', 'david', 'carolina']
+# message = "{},🧙 that was a great trick!"
+# message2 = "Hope to see you in the next challenge, {}\n"
+# for magician in magicians:
+#     print(message.format(magician.title()))
+#     print(message2.format(magician.title()))
+
+# print("Next Magician Day is on April, 1")
+
+# Indentation Errors
+# for magician in magicians:
+# print(message.format(magician.title()))
+#     print(message2.format(magician.title()))
+
+# Code Indentation is too much important otherwaise we get error
+# if we didn't add space before code (indentation) after lope statment
+# we will never get error but codes after lope statement will run
+# only once
+# Forgetting to indent additional lines
+# for magician in magicians:
+#     print(message.format(magician.title()))
+#     print(message2.format(magician.title()))
+#
+# print("Next Magician Day is on April, 1")
+# NOTE:: If we indenting print() method for second time
+# for magician in magicians:
+#     print(magician)
+# print(magician)
+# ----------------------------
+# valid Syntax Python code but it goint to error logically
+# Logical Error
+
+
+# (:) colon after loop statment tells python to interpret the next
+# line a part of a loop.
