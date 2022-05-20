926d66da66a3f7bbed4b8d41a477b44c684c94b4
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-05-20 22:03:37 +0430

    Chapter4, Excersixe 5 is done - styleing code. going to next

---
+++ chapter4/styling_code.py
@@ -0,0 +1,10 @@
+# Alireza Mohseni
+# 20/May/2022
+# Python Crash Course
+
+# Styling Our code
+# For Imporve Readability of our code for others
+# 1:: PEP (Python Enhancement Proposal)
+# 2:: Indentation => 4 space === 1 Tab
+# 3:: Line length => 80 Characters
+# 4:: Blank Linke to organized program
