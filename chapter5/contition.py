37610c21399b3574e70f063aac55f1b8d2526bf7
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-05-20 23:14:54 +0430

    Chapter5, Excercise1 is done, going to next

---
+++ chapter5/contition.py
@@ -0,0 +1,45 @@
+# Alireza Mohseni 
+# 22/May/2022
+# Python Crash Course
+
+# Chapter 5 - If Statements
+# for example chage correctness of programm
+computers = ['dell', 'mac', 'toshiba', 'samsung']
+# for pc in computers:
+#     if 'dell' in pc:
+#         print(pc.upper())
+#         continue
+
+#     print(pc)
+
+# Condition Check
+# car = "bmw"
+# print(car == "bmw")
+# print("bmw" in car)
+
+# Ignoring case whem checking for equality
+# python is casesensitive then if "ali" AND "Ali" or different
+# if we check those are not equal so the solution is when user enter
+# the input we must to make it uppercase or lower case and it will 
+# #improve our code
+
+# Checking to inequality (!=) or (not in)
+# for pc in computers:
+#     if input("enter Value") != computers: # not in 
+#         print("Value not Match!!!")
+#         print("Try Agin \n")
+#     else:
+#         print("Value Match 🤭")
+#         break
+
+# Numerical Coparisons
+# age = 18
+# print(age == 18)
+
+# Checking multiple conditions
+# age_0 = 22
+# age_1 = 20
+# print(age_0 >= 21 and age_1 >= 21)
+# print(age_0 >= 21 or age_1 >= 21)
+
+
---
+++ chapter5/excercise.py
@@ -0,0 +1,17 @@
+# Conditions in Python
+# NOTE:: Excercise 1
+
+# name = "hamid"
+# print(car == "hamid") # True
+# print(car == "jawid") # False
+
+# names = ["Alireza", "Karim", "Ahmad"]
+# for name in names:
+#     input_value = input("enter a name: ").lower()
+
+#     if input_value == name.lower():
+#         print("your chess match!!! 😮‍💨")
+#         break
+#     else: 
+#         print("Not Match Sorry!!!")
+#         input_value = ""
