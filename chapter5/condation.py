675fea69e96c684c04d6b0be488776cbe59c2579
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-05-21 17:47:33 +0430

    Chapter 5, Excercise 2 is done. going to next

---
+++ chapter5/condation.py
@@ -0,0 +1,89 @@
+# Alireza Mohseni 
+# 22/May/2022
+# Python Crash Course
+
+# Chapter 5 - If Statements
+# for example chage correctness of programm
+computers = ['dell', 'mac', 'toshiba', 'samsung']
+# for pc in computers:
+#     if 'dell' in pc:
+#         print(pc.upper())
+#         continue
+
+#     print(pc)
+
+# Condition Check
+# car = "bmw"
+# print(car == "bmw")
+# print("bmw" in car)
+
+# Ignoring case whem checking for equality
+# python is casesensitive then if "ali" AND "Ali" or different
+# if we check those are not equal so the solution is when user enter
+# the input we must to make it uppercase or lower case and it will 
+# #improve our code
+
+# Checking to inequality (!=) or (not in)
+# for pc in computers:
+#     if input("enter Value") != computers: # not in 
+#         print("Value not Match!!!")
+#         print("Try Agin \n")
+#     else:
+#         print("Value Match 🤭")
+#         break
+
+# Numerical Coparisons
+# age = 18
+# print(age == 18)
+
+# Checking multiple conditions
+# age_0 = 22
+# age_1 = 20
+# print(age_0 >= 21 and age_1 >= 21)
+# print(age_0 >= 21 or age_1 >= 21)
+
+# Using Multiple elif Blocks
+# user_submit_type = "pro"
+# if user_submit_type == "student":
+#     university_email = input("Enter Your University Email Address: ")
+# elif user_submit_type == "teacher":
+#     university_email = input("Please Enter Your University Email: ")
+# elif user_submit_type == "community":
+#     print('Some rolls may be disbale for you !!! 😭')
+# elif user_submit_type == "pro":
+#     email = input("Enter Your Email Address: ")
+#     first_name = input("Enter Your First Name: ")
+#     last_name = input("Enter Your Last Name: ")
+#     print("😎 Link to download your software have been sent to your email address")
+#     print(f"💗 Thank you Dear, {first_name} {last_name}. have nice day")
+
+# Omitting the else Block
+# Python does not require an else block at the end of an if-elif chain
+# for value in range(1, 11):
+#     """Not require for else"""
+#     if value == 10:
+#         print("Maximam values of 10")
+#         continue
+#     elif value == 5:
+#         print("Middle of values")
+#         continue
+#     elif value == 1:
+#         print("Minimam of values 1")
+#         continue
+    
+#     print(value)
+
+# Testing Multiple Conditions
+# if-elif-else is powerfull but it just only for one test to pass
+# we use if statment for testing multiple conditions
+require_topices = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in require_topices:
+    print("Adding myshrooms.")
+if 'pepperoni' in require_topices:
+    print("Adding pepperoni.")
+if 'extra cheese' in require_topices:
+    print("Adding Extra cheese")
+
+print('\nFinised making you pizza')
+
--- chapter5/excercise.py
+++ chapter5/excercise.py
@@ -15,3 +15,42 @@
 #     else: 
 #         print("Not Match Sorry!!!")
 #         input_value = ""
+
+# def error_message(line_number: int, message: str) -> str:
+#     line = line_number + 1
+#     return f'(L{line:03d}) {message}'
+
+# print(error_message(21, "confirm the vlaue"))
+
+# NOTE:: Excercise 2
+
+# alien_color = input("Shoot to color point [red], [yellow], [green]: ")
+# point = 0
+# if alien_color == 'green':
+#     print("You have Earned {} points. 🥳".format(point + 5))
+# elif alien_color == 'yellow': 
+#     print("You have Earned {} points. 🥳".format(point + 10))
+# elif alien_color == 'red':
+#     print("You have Earned {} points. 🥳".format(point + 15))
+
+# person_age = int(input("enter your age: "))
+
+# if person_age <= 2:
+#     print(f"Person in {person_age} year is baby \n")
+# elif person_age >= 4 and person_age <= 13:
+#     print(f"Person in {person_age} year is Kid \n")
+# elif person_age >= 13 and person_age <= 20:
+#     print(f"Person in {person_age} year is teenager \n")
+# elif person_age >= 20 and person_age <= 65:
+#     print(f"Person in {person_age} year is adult \n")
+# elif person_age >= 65:
+#     print(f"Person in {person_age} year is an elder \n")
+
+fevorite_fruits = ['apple', 'orange', 'pinapple', 'banana']
+
+if 'apple' in fevorite_fruits:
+    print("You really like apple")
+if 'banana' in fevorite_fruits:
+    print('You really like banana')
+
+
