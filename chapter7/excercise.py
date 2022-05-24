612410d423692d3bc3ecdd8a61b5df76c72dbaaf
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-05-24 22:46:34 +0430

    Chapter 7, input() function on excercise 1 is done. goint to next

---
+++ chapter7/excercise.py
@@ -0,0 +1,26 @@
+# NOTE:: Excercise 1
+
+# Rental Car
+# favorite_car = input("Tell me about your rental car: ")
+# print(f"Let me see if i can find you a {favorite_car}")
+
+# Resturatnt Seating
+# message = "Wellcome to GOG resturant."
+# seating_max = 8
+
+# reserve_date = input("When you want to came: [2022/10/10]")
+# total_customer = int(input("Enter your maximam guests: "))
+# if total_customer > seating_max:
+#     message = "Sorry! 🙄 your guests out of rand."
+#     message += "\nOnly 8 person can join in one seat"
+#     print(message)
+# else:
+#     print(f"Table is ready on {reserve_date}")
+
+# Multiples of Ten:
+# user_value = int(input("Your your number"))
+# user_value = user_value % 10
+# if user_value == 0:
+#     print("Correct Your value can muliples of ten")
+# else:
+#     print("Can\'t muliples with then")
---
+++ chapter7/inputFunction.py
@@ -0,0 +1,21 @@
+# Alireza Mohseni
+#  24/May/2022
+# Python Crash course
+from datetime import datetime
+
+# Input Function
+
+# input() function pauses our program then wait until the user fill the input
+
+# message = "If you tell me who are you, i can 🤭 Personalize your envirnemnt"
+# message += "\nWhat is your name?"
+# print(message)
+
+# username = input("Enter your name: ")
+# message = "Hello there!, {} hope you are doing well."
+# print(message.format(username))
+
+
+# THE Modulo Operator
+# usefull toll for working with numerical information, which divids one numer with other then return the remainder
+# print(4 % 3) # return 1
